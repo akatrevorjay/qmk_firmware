1dfc9600278d8b489e82e616fc26df09e1985e8b
Trevor Joynson (trevorj) <github@skywww.net>
2017-03-06 20:57:35 -0800

    Trevfinity

--- keyboards/ergodox/config.h
+++ keyboards/ergodox/config.h
@@ -7,7 +7,7 @@
 #define MOUSEKEY_MAX_SPEED      10
 #define MOUSEKEY_WHEEL_DELAY 0
 
-#define TAPPING_TOGGLE  1
+#define TAPPING_TOGGLE 0
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
@@ -16,9 +16,9 @@
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+//#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+//#define LOCKING_RESYNC_ENABLE
 
 /* key combination for command */
 #define IS_COMMAND() ( \
--- keyboards/ergodox/keymaps/trevfinity/keymap.c
+++ keyboards/ergodox/keymaps/trevfinity/keymap.c
@@ -24,8 +24,8 @@ enum {
 
 // Tap Dance Declarations
 enum {
-	TD_ESC_CAPS = 0,
-	/*TD_LTOG,*/
+    TD_ESC_CAPS = 0,
+    /*TD_LTOG,*/
 };
 
 /*void dance_flsh_each(qk_tap_dance_state_t *state, void *user_data) {*/
@@ -50,17 +50,16 @@ enum {
 const uint16_t PROGMEM fn_actions[] = {
     ACTION_LAYER_TAP_KEY(FNMM, KC_GRV),          // FN0 - Momentary Layer 1 or tap for grave/tilde
     ACTION_LAYER_TAP_TOGGLE(FNMM),               // FN1 - Momentary Layer 1 or tap to toggle
-    ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),       // FN2 - Control key if held, Esc key if tapped
     ACTION_BACKLIGHT_INCREASE(),                 // FN3
     ACTION_BACKLIGHT_DECREASE(),                 // FN4
 
-    ACTION_MODS_TAP_KEY(MOD_LSFT, KC_LBRC),
-    ACTION_MODS_TAP_KEY(MOD_RSFT, KC_RBRC),
+    /*ACTION_MODS_TAP_KEY(MOD_LSFT, KC_LBRC),*/
+    /*ACTION_MODS_TAP_KEY(MOD_RSFT, KC_RBRC),*/
 
-    ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ESC),
-    ACTION_MODS_TAP_KEY(MOD_LCTL, KC_TAB),
+    /*ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ESC), */
+    /*ACTION_MODS_TAP_KEY(MOD_LCTL, KC_TAB), */
 
-    ACTION_LAYER_TAP_TOGGLE(EZ),
+    /*ACTION_LAYER_TAP_TOGGLE(EZ),           */
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -92,38 +91,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_FN0,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
     CTL_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
     GUI_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-    SFT_T(KC_9),    CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
+    KC_LSFT,    CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
     TT(FNMM),       KC_LGUI, KC_LGUI, KC_LALT, KC_LCTL,
     KC_HOME,        KC_END,
     TT(EZ),
     GUI_T(KC_BSPC), ALT_T(KC_DEL),  CTL_T(KC_ESC),
     // right hand
-    KC_FN0,         KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  KC_BSLS,
-    KC_UP,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_FN0,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSLS,
+    KC_EQL,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS,
     KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    MEH_T(KC_NO),   KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), SFT_T(KC_0),
+    MEH_T(KC_NO),   KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
     KC_LPRN,        KC_RPRN, KC_LBRC, KC_RBRC, KC_RCTL,
     KC_PGUP,        KC_LEAD,
     KC_PGDN,
     TT(NAV),        ALT_T(KC_ENT), GUI_T(KC_SPC)
-
-    /*KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_UP,              */
-    /*KC_BSLS, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_TRNS,           */
-    /*CTL_T(KC_ESC), KC_A, KC_R, KC_S, KC_T, KC_D,              */
-    /*KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, ALL_T(KC_NO),      */
-    /*KC_FN1, KC_LGUI, KC_LEFT,KC_RGHT,KC_TRNS,                 */
-    /*KC_HOME,KC_INS,                                           */
-    /*KC_DEL,                                                   */
-    /*KC_SPC,KC_BSPC,KC_LALT,                                   */
-    /*// right hand                                             */
-    /*KC_DOWN, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,            */
-    /*KC_TRNS, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS,        */
-    /*KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT,                    */
-    /*MEH_T(KC_NO),KC_K, KC_M, KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,*/
-    /*KC_LPRN,KC_RPRN,KC_LBRC,KC_RBRC, KC_FN1,                  */
-    /*KC_PGUP, KC_END,                                          */
-    /*KC_PGDN,                                                  */
-    /*KC_RALT,KC_TAB, KC_ENT                                    */
 ),
 
 [WM] = KEYMAP(  // layer 0 : default
@@ -174,8 +155,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_HOME,
     KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_END,
-    KC_TRNS, KC_TRNS,   KC_AT, KC_EXLM, KC_TRNS,
-    KC_FN3,  KC_FN4,
+    KC_TRNS, KC_TRNS, KC_AT, KC_EXLM, KC_TRNS,
+    KC_TRNS, KC_TRNS,
     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
