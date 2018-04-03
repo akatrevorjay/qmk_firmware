e87cf9bb2cc597347897b984f49c86971aa5e0cf
Trevor Joynson (trevorj) <github@skywww.net>
2018-04-03 08:20:18 -0700

    These are fine

--- layouts/community/ergodox/trevfinity/rules.mk
+++ layouts/community/ergodox/trevfinity/rules.mk
@@ -13,10 +13,10 @@ CONSOLE_ENABLE   = no  # Console for debug(+400)
 COMMAND_ENABLE   = yes  # Commands for debug and configuration
 NKRO_ENABLE      = yes # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ # nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-MIDI_ENABLE      = yes  # MIDI controls
+MIDI_ENABLE      = yea  # MIDI controls
 AUDIO_ENABLE     = no  # Audio output on port C6
 UNICODE_ENABLE   = yes  # Unicode
-BLUETOOTH_ENABLE = no  # Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = yea  # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE  = no  # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 
